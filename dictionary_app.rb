e3fe95b597004833462785163b022fa689ec1795
mellowjoeys <mellowjoey@gmail.com>
2019-08-26 10:09:45 -0500

    dictionary updates

--- dictionary_app.rb
+++ dictionary_app.rb
@@ -3,15 +3,39 @@ require "http"
 p "Please enter a word you want to look up."
 word = gets.chomp
 
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=9f63510ab680a9a2504370c0a530c846ba454b12337f0c6c0")
+example_response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=9f63510ab680a9a2504370c0a530c846ba454b12337f0c6c0")
 
-definition = response.parse[0]["text"]
+pronunciation_response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/pronunciations?useCanonical=false&limit=50&api_key=9f63510ab680a9a2504370c0a530c846ba454b12337f0c6c0")
+
+top_example_response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/topExample?useCanonical=false&api_key=9f63510ab680a9a2504370c0a530c846ba454b12337f0c6c0")
+
+definition_response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=9f63510ab680a9a2504370c0a530c846ba454b12337f0c6c0")
+
+definition = definition_response.parse[0]["text"]
 
 p definition
 
-example_response = HTTP.get("https://api.wordnik.com/v4/word.json/hello/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=9f63510ab680a9a2504370c0a530c846ba454b12337f0c6c0")
 
-example = response.parse[0][]
+example = example_response.parse["examples"][0]["text"]
+
+# OR
+
+# definition = example_response.parse
+# definitions.each do |definition|
+#   puts "#{definition["text"]}"
+# end
+
+p example
+
+pronunciations = pronunciation_response.parse
+
+pronunciations.each do |pronunciation|
+  p "#{pronunciation["raw"]}"
+end
+
+top_example = top_example_response.parse["text"]
+p top_example
+
 # examples.each do |example|
 #   p example
 #   puts ""
